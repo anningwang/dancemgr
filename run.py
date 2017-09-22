d4ff9b7394ef720925c5e310e513d8d217348900
anningwang <wxg001@21cn.com>
2017-09-22 11:46:56 +0800

    add file: getip.py

--- run.py
+++ run.py
@@ -1,12 +1,10 @@
 #!flask/bin/python
 # -*- coding:utf-8 -*-
 from app import app
+from app.tools.getip import get_ip
 
-import socket
-hostname = socket.gethostname()
-ip = socket.gethostbyname(hostname)
 
-# app.run(debug=True)
 if __name__ == '__main__':
+    ip = get_ip()
     # app.run(host=ip, port=80, debug=True, use_reloader=False, threaded=True)
     app.run(host=ip, port=8081, debug=True, threaded=True)
