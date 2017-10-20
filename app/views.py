c04cd1c16a04bbad26b98c7a16bf48ea0042009f
anningwang <wxg001@21cn.com>
2017-10-21 00:46:17 +0800

    删除无用表格，更换favicon.icon

--- app/views.py
+++ app/views.py
@@ -1,18 +1,16 @@
 # -*- coding:utf-8 -*-
-from flask import render_template, flash, redirect, session, url_for, request, g, jsonify, json, Response
+from flask import render_template, flash, redirect, url_for, request, g, jsonify, json
 from flask_login import login_user, logout_user, current_user, login_required
 from flask_sqlalchemy import get_debug_queries
-from flask_babel import gettext
-from app import app, db, lm, oid, babel
-from forms import EditForm, SearchForm, DanceLoginForm, DanceRegistrationForm
-from models import User, ROLE_USER, ROLE_ADMIN, Post, DanceStudent, DanceClass, DanceSchool, DanceUser,\
+from app import app, db, lm, babel
+from forms import DanceLoginForm, DanceRegistrationForm
+from models import ROLE_ADMIN, DanceStudent, DanceClass, DanceSchool, DanceUser,\
     DanceStudentClass, DanceCompany, DanceUserSchool, DcShowDetailFee, DcCommFeeMode, DcShowRecpt, DcFeeItem,\
     DanceOtherFee, DanceReceipt, DanceClassReceipt, DanceTeaching, DcClassType, DanceTeacher, DanceTeacherEdu,\
     DanceTeacherWork, DcCommon, DanceCourse, DanceCourseItem, DanceClassRoom, UpgradeClass, UpgClassItem
 from datetime import datetime
-from emails import follower_notification
 from translate import microsoft_translate
-from config import POSTS_PER_PAGE, MAX_SEARCH_RESULTS, LANGUAGES, DATABASE_QUERY_TIMEOUT
+from config import LANGUAGES, DATABASE_QUERY_TIMEOUT
 from dcglobal import *
 from tools.tools import dc_records_changed
 from sqlalchemy import func
@@ -31,8 +29,6 @@ def get_locale():
 @app.before_request
 def before_request():
     g.user = current_user
-    if g.user.is_authenticated:
-        g.search_form = SearchForm()
     g.locale = get_locale()
 
 
@@ -65,33 +61,6 @@ def index():
     return render_template('index.html',
                            title='Home')
 
-'''
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
-@app.route('/index/<int:page>', methods=['GET', 'POST'])
-@login_required
-def index(page=1):
-    form = PostForm()
-    if form.validate_on_submit():
-        language = guessLanguage(form.post.data)
-        if language == 'UNKNOWN' or len(language) > 5:
-            language = ''
-        post = Post(body=form.post.data,
-                    timestamp=datetime.utcnow(),
-                    author=g.user,
-                    language=language)
-        db.session.add(post)
-        db.session.commit()
-        flash(gettext('Your post is now live!'))
-        return redirect(url_for('index'))
-    g.user.followed_posts()
-    posts = g.user.followed_posts().paginate(page, POSTS_PER_PAGE, False)
-    return render_template('index.html',
-                           title='Home',
-                           form=form,
-                           posts=posts)
-'''
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -113,48 +82,6 @@ def login():
 
     return render_template('login.html', form=form,
                            username='' if form.username.data is None else form.username.data)
-
-'''
-@app.route('/login', methods=['GET', 'POST'])
-@oid.loginhandler
-def login():
-    if g.user is not None and g.user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        session['remember_me'] = form.remember_me.data
-        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
-    return render_template('login.html',
-                           title='Sign In',
-                           form=form,
-                           providers=app.config['OPENID_PROVIDERS'])
-'''
-
-
-@oid.after_login
-def after_login(resp):
-    if resp.email is None or resp.email == "":
-        flash(gettext('Invalid login. Please try again.'))
-        return redirect(url_for('login'))
-    username = User.query.filter_by(email=resp.email).first()
-    if username is None:
-        nickname = resp.nickname
-        if nickname is None or nickname == "":
-            nickname = resp.email.split('@')[0]
-        nickname = User.make_valid_nickname(nickname)
-        nickname = User.make_unique_nickname(nickname)
-        username = User(nickname=nickname, email=resp.email, role=ROLE_USER)
-        db.session.add(username)
-        db.session.commit()
-        # make the user follow him/herself
-        db.session.add(user.follow(username))
-        db.session.commit()
-    remember_me = False
-    if 'remember_me' in session:
-        remember_me = session['remember_me']
-        session.pop('remember_me', None)
-    login_user(username, remember=remember_me)
-    return redirect(request.args.get('next') or url_for('index'))
 
 
 @app.route('/logout')
@@ -192,120 +119,6 @@ def register():
         return redirect(url_for('login'))
 
     return render_template('register.html', form=form)        # dance_register
-
-
-@app.route('/favicon.ico')
-def response_favicon():
-    import os
-
-    curdir = os.path.abspath(os.path.dirname(__file__))
-    fn = os.path.join(curdir, 'static/img/favicon.ico')
-    image = file(fn)
-    resp = Response(image, mimetype="image/jpeg")
-    return resp
-
-
-@app.route('/user/<nickname>')
-@app.route('/user/<nickname>/<int:page>')
-@login_required
-def user(nickname, page=1):
-    username = User.query.filter_by(nickname=nickname).first()
-    if username is None:
-        flash(gettext('User %(nickname)s not found.', nickname=nickname))
-        return redirect(url_for('index'))
-    posts = username.posts.paginate(page, POSTS_PER_PAGE, False)
-    return render_template('user.html', user=username, posts=posts)
-
-
-@app.route('/edit', methods=['GET', 'POST'])
-@login_required
-def edit():
-    form = EditForm(g.user.nickname)
-    if form.validate_on_submit():
-        g.user.nickname = form.nickname.data
-        g.user.about_me = form.about_me.data
-        db.session.add(g.user)
-        db.session.commit()
-        flash(gettext('Your changes have been saved.'))
-        return redirect(url_for('edit'))
-    elif request.method != "POST":
-        form.nickname.data = g.user.nickname
-        form.about_me.data = g.user.about_me
-    return render_template('edit.html', form=form)
-
-
-@app.route('/follow/<nickname>')
-@login_required
-def follow(nickname):
-    username = User.query.filter_by(nickname=nickname).first()
-    if username is None:
-        flash('User ' + nickname + ' not found.')
-        return redirect(url_for('index'))
-    if username == g.user:
-        flash(gettext('You can\'t follow yourself!'))
-        return redirect(url_for('user', nickname=nickname))
-    u = g.user.follow(username)
-    if u is None:
-        flash(gettext('Cannot follow %(nickname)s.', nickname=nickname))
-        return redirect(url_for('user', nickname=nickname))
-    db.session.add(u)
-    db.session.commit()
-    flash(gettext('You are now following %(nickname)s!', nickname=nickname))
-    follower_notification(username, g.user)
-    return redirect(url_for('user', nickname=nickname))
-
-
-@app.route('/unfollow/<nickname>')
-@login_required
-def unfollow(nickname):
-    username = User.query.filter_by(nickname=nickname).first()
-    if username is None:
-        flash('User ' + nickname + ' not found.')
-        return redirect(url_for('index'))
-    if username == g.user:
-        flash(gettext('You can\'t unfollow yourself!'))
-        return redirect(url_for('user', nickname=nickname))
-    u = g.user.unfollow(username)
-    if u is None:
-        flash(gettext('Cannot unfollow %(nickname)s.', nickname=nickname))
-        return redirect(url_for('user', nickname=nickname))
-    db.session.add(u)
-    db.session.commit()
-    flash(gettext('You have stopped following %(nickname)s.', nickname=nickname))
-    return redirect(url_for('user', nickname=nickname))
-
-
-@app.route('/delete/<int:pid>')
-@login_required
-def delete(pid):
-    post = Post.query.get(pid)
-    if post is None:
-        flash('Post not found.')
-        return redirect(url_for('index'))
-    if post.author.id != g.user.id:
-        flash('You cannot delete this post.')
-        return redirect(url_for('index'))
-    db.session.delete(post)
-    db.session.commit()
-    flash('Your post has been deleted.')
-    return redirect(url_for('index'))
-
-
-@app.route('/search', methods=['POST'])
-@login_required
-def search():
-    if not g.search_form.validate_on_submit():
-        return redirect(url_for('index'))
-    return redirect(url_for('search_results', query=g.search_form.search.data))
-
-
-@app.route('/search_results/<query>')
-@login_required
-def search_results(query):
-    results = Post.query.whoosh_search(query, MAX_SEARCH_RESULTS).all()
-    return render_template('search_results.html',
-                           query=query,
-                           results=results)
 
 
 @app.route('/translate', methods=['POST'])
