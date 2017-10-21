9215854280827fa07a4a99e5ba10deb58dfa46bc
anningwang <wxg001@21cn.com>
2017-10-21 18:44:14 +0800

    设置python编码为utf8

--- run.py
+++ run.py
@@ -2,13 +2,9 @@
 # -*- coding:utf-8 -*-
 from app import app
 from app.tools.getip import get_ip
-import sys
 
 
 if __name__ == '__main__':
-    reload(sys)
-    sys.setdefaultencoding('utf8')
-
     ip = get_ip()
     # app.run(host=ip, port=80, debug=True, use_reloader=False, threaded=True)
     app.run(host=ip, port=8081, debug=True, threaded=True)
