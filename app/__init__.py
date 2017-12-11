0219d1c7b11b3e19e8b2bf79ef4d09824bccb377
anningwang <wxg001@21cn.com>
2017-12-11 17:09:36 +0800

    app name 从 config 文件中获取。

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from flask_login import LoginManager
 from flask_openid import OpenID
 from flask_mail import Mail
 from flask_babel import Babel, lazy_gettext
-from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD, FILENAME_APP_LOG
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD, FILENAME_APP_LOG, APP_NAME
 from momentjs import momentjs
 from datetime import timedelta
 
@@ -42,7 +42,7 @@ if not app.debug:
     if MAIL_USERNAME or MAIL_PASSWORD:
         credentials = (MAIL_USERNAME, MAIL_PASSWORD)
     mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS,
-                               'dancemgr failure', credentials)
+                               APP_NAME + ' failure', credentials)
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
 
@@ -54,7 +54,7 @@ if not app.debug:
     file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     app.logger.addHandler(file_handler)
     app.logger.setLevel(logging.INFO)
-    app.logger.info('dancemgr startup')
+    app.logger.info(APP_NAME + ' startup')
 
 app.jinja_env.globals['momentjs'] = momentjs
 
